f0a2a99f8cf380ae8d8eb84fc6859461f5a689c9
oliveirajulio <juliocesargomes33@gmail.com>
2025-01-08 15:07:41 -0300

    mvp

--- src/pages/Item/index.js
+++ src/pages/Item/index.js
@@ -45,7 +45,7 @@ function Item() {
             setAllData(allDataFetched); // Salva todos os dados
             setSelectedItem(itemEncontrado); // Salva o item único
             setLoading(false);
-          }, 3000);
+          }, 1500);
         } catch (err) {
           setError(err.message);
           setLoading(false);
