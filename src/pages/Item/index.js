63522cf5511914b0ab0597fe3315ac4009863ace
oliveirajulio <juliocesargomes33@gmail.com>
2025-01-08 15:48:35 -0300

    mvp5

--- src/pages/Item/index.js
+++ src/pages/Item/index.js
@@ -45,7 +45,7 @@ function Item() {
             setAllData(allDataFetched); // Salva todos os dados
             setSelectedItem(itemEncontrado); // Salva o item único
             setLoading(false);
-          }, 1200);
+          }, 1100);
         } catch (err) {
           setError(err.message);
           setLoading(false);
