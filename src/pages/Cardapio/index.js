63ec118a623960a2f2f29472ea5f33ee6cd184da
oliveirajulio <juliocesargomes33@gmail.com>
2025-02-16 17:41:00 -0300

    add itemcardapio page

--- src/pages/Cardapio/index.js
+++ src/pages/Cardapio/index.js
@@ -12,6 +12,7 @@ import WineBarIcon from "@mui/icons-material/WineBar";
 import ImageNotSupportedIcon from "@mui/icons-material/ImageNotSupported";
 
 function Cardapio() {
+    const navigate = useNavigate();
   const [datacardapio, setDataCardapio] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -57,6 +58,10 @@ function Cardapio() {
     return <p>Erro: {error}</p>;
   }
 
+  const passcod = (id) => {
+    navigate(`/product/${id}`);
+  };
+
   return (
     <div className="container-cardapio">
       <div className="main-header-cardapio"></div>
@@ -64,12 +69,12 @@ function Cardapio() {
       <div className="main-filter-cardapio">
         <nav className="nav-cardapio">
           <ul>
-            <button onClick={() => filtrarPorCategoria("Lanche")}><LocalDiningIcon className="ic-filter"/></button>
+            <button onClick={() => filtrarPorCategoria("Lanches")}><LocalDiningIcon className="ic-filter"/></button>
             <button onClick={() => filtrarPorCategoria("Bolos/Tortas/Sobremesas")}><CakeIcon className="ic-filter"/></button>
             <button onClick={() => filtrarPorCategoria("Doses")}><WaterDropIcon className="ic-filter"/></button>
             <button onClick={() => filtrarPorCategoria("Bebidas")}><FastfoodIcon className="ic-filter"/></button>
             <button onClick={() => filtrarPorCategoria("Café e Chocolates")}><CoffeeIcon className="ic-filter"/></button>
-            <button onClick={() => filtrarPorCategoria("Marmitinha e Torta")}><WineBarIcon className="ic-filter"/></button> 
+            <button onClick={() => filtrarPorCategoria("Marmitinha e Tortas")}><WineBarIcon className="ic-filter"/></button> 
           </ul>
           <h3 className="filter-name">{categoriaSelecionada ? `${categoriaSelecionada}` : "Escolha um filtro"}</h3>
         </nav>
@@ -79,7 +84,8 @@ function Cardapio() {
         <ul className="item-list-cardapio">
           {produtosFiltrados.map((item, index) => (
             <li key={index} className="item-row-cardapio">
-              <button className="item-button-cardapio">
+              <button className="item-button-cardapio"
+                       onClick={() => passcod(item.Código)}>
                 <span className="icon-cardapio">
                   <ImageNotSupportedIcon className="ic-cardapio" />
                 </span>
